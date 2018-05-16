ffb41e5d772a32efebb54ed9db5748cb8afb0062
Zahan Malkani <zahanm@fb.com>
2018-05-16 11:38:01 -0700

    stderr to /dev/null since one of the .hg/ files can be missing

--- hg_branch_or_bookmark.fish
+++ hg_branch_or_bookmark.fish
@@ -1,4 +1,4 @@
 function hg_branch_or_bookmark -d "Get current branch / bookmark name" -a repo_root
   command cat $repo_root'/.hg/bookmarks.current' ^/dev/null;
-    or command cat $repo_root'/.hg/branch'
+    or command cat $repo_root'/.hg/branch' ^/dev/null
 end
